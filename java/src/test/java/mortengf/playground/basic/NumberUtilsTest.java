90658c719fb5714daa6f8d5528a5ccbffa303a8a
Morten Garbøl Franck <mgf@dexi.io>
2017-11-30 12:00:40 +0100

    Added test to show that 'NumberUtils' from Apache's commons-lang is buggy in version 2.6

---
+++ java/src/test/java/mortengf/playground/basic/NumberUtilsTest.java
@@ -0,0 +1,12 @@
+package mortengf.playground.basic;
+
+import org.apache.commons.lang.math.NumberUtils;
+
+public class NumberUtilsTest {
+    public static void main(String[] args) {
+        String number = "2.5L";
+        boolean isNumber = NumberUtils.isNumber(number);
+        System.out.println(isNumber);
+        NumberUtils.createNumber(number);
+    }
+}
