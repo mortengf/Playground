bc9a1abafdb7a6ff681de6985c57841929eaef61
Morten Garbøl Franck <mgf@dexi.io>
2025-02-03 13:15:30 +0100

    Add LanguageDataset class for convenience

--- python/ml/square/tutorial.py
+++ python/ml/square/tutorial.py
@@ -12,6 +12,39 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader, random_split
 
+class LanguageDataset(Dataset):
+    """
+    An extension of the Dataset object to:
+      - Make training loop cleaner
+      - Make ingestion easier from pandas df's
+    """
+    def __init__(self, df, tokenizer):
+        self.labels = df.columns
+        self.data = df.to_dict(orient='records')
+        self.tokenizer = tokenizer
+        x = self.fittest_max_length(df)
+        self.max_length = x
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = self.data[idx][self.labels[0]]
+        y = self.data[idx][self.labels[1]]
+        text = f"{x} | {y}"
+        tokens = self.tokenizer.encode_plus(text, return_tensors='pt', max_length=128, padding='max_length', truncation=True)
+        return tokens
+
+    def fittest_max_length(self, df):
+      """
+      Smallest power of two larger than the longest term in the data set.
+      Important to set up max length to speed training time.
+      """
+      max_length = max(len(max(df[df.columns[0]], key=len)), len(max(df[df.columns[1]], key=len)))
+      x = 2
+      while x < max_length: x = x * 2
+      return x
+
 # Load data set from huggingface
 dataset = load_dataset("erwanlc/cocktails_recipe_no_brand")
 
@@ -42,3 +75,15 @@ model = GPT2LMHeadModel.from_pretrained('distilgpt2').to(device)
 
 # Model params
 BATCH_SIZE = 8
+
+# Cast the Huggingface data set as a LanguageDataset we defined above
+dataset = LanguageDataset(df, tokenizer)
+
+# Create train, valid
+train_size = int(0.8 * len(dataset))
+valid_size = len(dataset) - train_size
+train_data, valid_data = random_split(dataset, [train_size, valid_size])
+
+# Make the iterators
+train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+valid_loader = DataLoader(valid_data, batch_size=BATCH_SIZE)
