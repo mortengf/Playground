901e2fe450afa9362b665aa1cd77fcb04c06ac7d
Morten Garbøl Franck <mgf@dexi.io>
2025-02-03 13:23:17 +0100

    Add training loop

--- python/ml/square/tutorial.py
+++ python/ml/square/tutorial.py
@@ -87,3 +87,75 @@ train_data, valid_data = random_split(dataset, [train_size, valid_size])
 # Make the iterators
 train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 valid_loader = DataLoader(valid_data, batch_size=BATCH_SIZE)
+
+# Set the number of epochs
+num_epochs = 3
+
+# Training parameters
+batch_size = BATCH_SIZE
+model_name = 'distilgpt2'
+gpu = 0
+
+# Set the learning rate and loss function
+## CrossEntropyLoss measures how close answers to the truth.
+## More punishing for high confidence wrong answers
+criterion = nn.CrossEntropyLoss(ignore_index = tokenizer.pad_token_id)
+optimizer = optim.Adam(model.parameters(), lr=5e-4)
+tokenizer.pad_token = tokenizer.eos_token
+
+# Init a results dataframe
+results = pd.DataFrame(columns=['epoch', 'transformer', 'batch_size', 'gpu',
+                                'training_loss', 'validation_loss', 'epoch_duration_sec'])
+# The training loop
+for epoch in range(num_epochs):
+    start_time = time.time()  # Start the timer for the epoch
+
+    # Training
+    ## This line tells the model we're in 'learning mode'
+    model.train()
+    epoch_training_loss = 0
+    train_iterator = tqdm(train_loader, desc=f"Training Epoch {epoch+1}/{num_epochs} Batch Size: {batch_size}, Transformer: {model_name}")
+    for batch in train_iterator:
+        optimizer.zero_grad()
+        inputs = batch['input_ids'].squeeze(1).to(device)
+        targets = inputs.clone()
+        outputs = model(input_ids=inputs, labels=targets)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+        train_iterator.set_postfix({'Training Loss': loss.item()})
+        epoch_training_loss += loss.item()
+    
+    avg_epoch_training_loss = epoch_training_loss / len(train_iterator)
+
+    # Validation
+    ## This line below tells the model to 'stop learning'
+    model.eval()
+    epoch_validation_loss = 0
+    total_loss = 0
+    valid_iterator = tqdm(valid_loader, desc=f"Validation Epoch {epoch+1}/{num_epochs}")
+    with torch.no_grad():
+        for batch in valid_iterator:
+            inputs = batch['input_ids'].squeeze(1).to(device)
+            targets = inputs.clone()
+            outputs = model(input_ids=inputs, labels=targets)
+            loss = outputs.loss
+            total_loss += loss
+            valid_iterator.set_postfix({'Validation Loss': loss.item()})
+            epoch_validation_loss += loss.item()
+
+    avg_epoch_validation_loss = epoch_validation_loss / len(valid_loader)
+
+    end_time = time.time()  # End the timer for the epoch
+    epoch_duration_sec = end_time - start_time  # Calculate the duration in seconds
+
+    new_row = {'transformer': model_name,
+               'batch_size': batch_size,
+               'gpu': gpu,
+               'epoch': epoch+1,
+               'training_loss': avg_epoch_training_loss,
+               'validation_loss': avg_epoch_validation_loss,
+               'epoch_duration_sec': epoch_duration_sec}  # Add epoch_duration to the dataframe
+
+    results.loc[len(results)] = new_row
+    print(f"Epoch: {epoch+1}, Validation Loss: {total_loss/len(valid_loader)}")
