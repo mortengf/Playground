3d57a50acbb6c3266c654d1655c372a51bb6917f
Morten Garbøl Franck <mgf@dexi.io>
2025-02-07 12:23:03 +0100

    More "length of last words" examples

--- typescript/length-of-last-word.ts
+++ typescript/length-of-last-word.ts
@@ -1,4 +1,13 @@
-// https://leetcode.com/problems/length-of-last-word/
+/*
+https://leetcode.com/problems/length-of-last-word/
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of only English letters and spaces ' '.
+    There will be at least one word in s.
+
+*/
 
 function lengthOfLastWord(s: string): number {
     let allTokens: string[] = s.split(/\s+/);
@@ -12,3 +21,9 @@ function lengthOfLastWord(s: string): number {
 
 let input1 = "Hello World";
 console.log(lengthOfLastWord(input1));
+
+let input2 = "   fly me   to   the moon  ";
+console.log(lengthOfLastWord(input2));
+
+let input3 = "luffy is still joyboy";
+console.log(lengthOfLastWord(input3));
