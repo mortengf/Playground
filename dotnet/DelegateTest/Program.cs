6aa843bc2b7dad037e5976fc21722d8605454a5a
Morten Garbøl Franck <mgf@dexi.io>
2025-03-25 12:50:57 +0100

    Add comments

--- dotnet/DelegateTest/Program.cs
+++ dotnet/DelegateTest/Program.cs
@@ -1,26 +1,33 @@
 ﻿
 
 public class Program {
+    
+    // Declare the delegate aka callback function
     public delegate int TheBestDelegate(string message);
 
+    // Implement the delegate/callback function
+    public static int PrintTheBestMessage(string message) {
+        Console.WriteLine(message + " og delegates er det bedste!");
+        return 10;
+    }
+
     public static void Main(string[] args) {
-        // Standard syntax
+        // Instantiate the delegate - standard syntax
         TheBestDelegate myDelegate = new TheBestDelegate(PrintTheBestMessage);
+
+        // Invoke the delegate
         int literNissebrusTilbage = myDelegate("Nissebrus");
         Console.WriteLine("Der er " + literNissebrusTilbage + " liter Nissebrus tilbage");
 
-        // Lambda syntax
+        // Instantiate the delegate - lambda syntax (reuse existing object)
         myDelegate = (message) => { 
             Console.WriteLine(message + " og lambda-style delegates er det bedste!");
             return 8;
         };
+
+        // Invoke the delegate again
         int chiliCheeseRingsTilbage = myDelegate("Chili-Cheese Rings");
         Console.WriteLine("Der er " + chiliCheeseRingsTilbage + " poser Chili-Cheese Rings tilbage");        
-    }
-    
-    public static int PrintTheBestMessage(string message) {
-        Console.WriteLine(message + " og delegates er det bedste!");
-        return 10;
     }
 
 }
